8e3eb3b602e8e7b92b5400cd96bef10779a67e50
ulzirok <ulzirok@gmail.com>
2025-09-06 10:57:38 +0500

    add background image to the base page

--- src/app/components/base/base.ts
+++ src/app/components/base/base.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-base',
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './base.html',
   styleUrl: './base.scss'
 })
